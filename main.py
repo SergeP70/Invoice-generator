fe6339efec3eac3b48b7ca44547eea4b6631b19f
sergeP70 <serge@pille.be>
2025-01-04 08:59:50 +0100

    Load data from excel into dataframes

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-# This is a sample Python script.
+# = Automation program
+import pandas as pd
+import os
+import glob
+
+
+filepaths = glob.glob('invoices/*.xlsx')
+for filepath in filepaths:
+    df = pd.read_excel(filepath)
+    print(df)
+
+
+
+
+
+
 
